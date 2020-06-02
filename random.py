11fae254baad9e7813bb8debde5172dd52c20628
Matthew Joseph <matthew.j.joe@gmail.com>
2020-06-01 21:45:01 -0400

    Add files via upload

---
+++ random.py
@@ -0,0 +1,25 @@
+# Python syntax
+
+#NOTES: PY is case sensitive: ie- print and Print are differnt in syntax!
+
+#Strings can be printed as such. Single quotes or double quotes can work
+print("Hello World!")
+
+#Python variables don't need a decleration keyword. Just the variable name and value
+todays_date = "11/11/11"
+
+#Arithemtic: Python let you do the basics (*,/,+,-) BUT, you also have the modulo opeator 
+is_this_number_odd = 15 % 2 
+is_this_number_even = 133 % 7 
+
+#Note: still req to put var in () when printing it to the log
+print (is_this_number_even)
+
+#A number with a decimal point is called a floatin python 
+regNum = 1003993
+floatNum = 1.300
+
+
+
+
+
