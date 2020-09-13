2758b0a27017506ce9f3b224425643318702a080
Matthew Joseph <matthew.j.joe@gmail.com>
2020-09-13 10:15:35 -0400

    Update random.py

--- random.py
+++ random.py
@@ -24,3 +24,24 @@ floatNum = 1.300
 # - SyntaxError means there is something wrong with the way your program is written — punctuation that does not belong, a command where it is not expected, or a missing parenthesis can all trigger a SyntaxError.
 # - A NameError occurs when the Python interpreter sees a word it does not recognize. Code that contains something that looks like a variable but was never defined will throw a NameError
 
+#Modulo: py offers a companion to the division operator called the Modulo ( % ).
+print(29 % 5) -> this would give us 4 because 29/5 is 5 with a REMAINDER OF 4
+
+#Concatenation: This is simply using the ' + ' operator to add strings, integers, etc together. 
+greeting_text = "Hey there!"
+question_text = "How are you doing?"
+full_text = greeting_text + question_text
+# Prints "Hey there!How are you doing?"
+- OR YOU CAN USE ( += )
+full_text += "This will be added to the end of what full_text is already assined to"
+print(full_text)
+
+#Multi-line strings
+mutli_line = """
+This 
+Spans
+Four 
+Lines
+"""
+
+#Functions
